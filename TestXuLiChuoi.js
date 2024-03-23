7df4971eb91e53ad530c6865dd19fa181658f998
user <user@users-MacBook-Pro.local>
2024-03-24 01:12:57 +0700

    xoa khoang trang

--- TestXuLiChuoi.js
+++ TestXuLiChuoi.js
@@ -1,4 +1,4 @@
-let firstName = "tien   "
+let firstName = "   tien   "
 
 let nameHai = "My name Is :"
 let nameLength = nameHai.length
@@ -6,7 +6,12 @@ let nameLength = nameHai.length
 let caHai = nameHai + firstName
 // console.log(caHai)
 
-let string2 = nameHai.concat(fistName)
+let string2 = nameHai.concat(firstName)
 
 // console.log(string2)
 
+// let khoangTrang = nameHai + firstName
+// console.log(khoangTrang)
+let khoangTrang1 = firstName.trim()
+let khoangTrang = nameHai + khoangTrang1
+console.log(khoangTrang)
