1fb8ae8b42d5329bf3de9f7545084cc1c458a1dc
user <user@users-MacBook-Pro.local>
2024-03-24 00:55:19 +0700

    xulichuoi

---
+++ TestXuLiChuoi.js
@@ -0,0 +1,12 @@
+let firstName = "tien   "
+
+let nameHai = "My name Is :"
+let nameLength = nameHai.length
+// console.log(nameLength)
+let caHai = nameHai + firstName
+// console.log(caHai)
+
+let string2 = nameHai.concat(fistName)
+
+// console.log(string2)
+
