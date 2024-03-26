ca5ba687171b947dadb3c0e13482c4ebd2256829
user <user@users-MacBook-Pro.local>
2024-03-26 23:19:31 +0700

    fixx

---
+++ xulimang.js
@@ -0,0 +1,28 @@
+// let name1 = '    Tien'
+
+// let name2 = " My name is:"
+
+
+// let culi = name1.trim()
+// let noichuoi = name2.concat(culi)
+
+
+// let cat = culi.split("")
+
+// let xcat= cat.indexOf("i")
+// console.log(xcat)
+
+
+let myArray = [ 1, "anh" , 2 , "Tien"]
+
+myArray.push(3,"bui")
+
+myArray.pop()
+
+myArray.splice(2,3)
+
+myArray[1] = 'bui'
+
+let indexx = myArray.indexOf(1)
+
+console.log(indexx)
