6af8a6ede5a47fb96d852f52797bbcfb0d8ea6b5
user <user@users-MacBook-Pro.local>
2024-03-31 17:23:53 +0700

    new commit

--- bt/tonghop.js
+++ bt/tonghop.js
@@ -1,13 +1,11 @@
 // let myArray = [1,2,3,4,5,6,7,8,9,10]
 
-// let mangchan = [
-
-// ]
+// let mangchan = []
 // let mangle = []
 
 // for (let i = 0; i < myArray.length; i++) {
 //     const element = myArray[i];
-//     if (element %2 ==0) {
+//     if (element % 2 ==0) {
 //         mangchan.push(element)
 //     } else {
 //         mangle.push(element)
@@ -17,39 +15,24 @@
 // console.log(mangchan)
 // console.log(mangle)
 
-
 // class Person {
-//     constructor(name , age){
-//         this.name = name
-//         this.age= age
-//     }
+//     // constructor(name , age){
+//     //         this.name =name
+//     //         this.age = age
+//     // }
 
-//     getInfo(){
-//         console.log('My name is:',this.name)
-//         console.log('My age :',this.age)
+//     getInfo(Name , age){
+//         console.log('My name :',Name)
+//         console.log('My age:',age)
 //     }
 // }
-// let ti = new Person('tien',18)
-// ti.getInfo()
+// let teo = new Person()
+// teo.getInfo()
 
-// let name1 = "   Tien"
+
+// let name1 = '   Tien'
+
 // let name2 = "My name is:"
-// let khudau = name1.trim()
-// let congchuoi =  name2.concat(khudau)
-
-// let catchuoi = khudau.split("")
-
-
-// let index = khudau.indexOf('u')
-// console.log(index)
-
-// let myArray = [1,'tien',2,'anh',3,'bui']
-
-// myArray.push('ong')
-// myArray.pop()
-
-// myArray[1] = 'bui'
-// myArray[5] = 'tien'
-
-// myArray.splice(0,5)
-// console.log(myArray)
+// let xulidau = name1.trim()
+// let congchuoi = name2.concat(xulidau)
+// console.log(congchuoi)
